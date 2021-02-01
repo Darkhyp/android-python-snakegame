0edf55d3bdfb078bf1c0c1993c457a7b17cf2136
Darkhyp <a_v_kor73@rambler.ru>
2021-02-01 16:33:51 +0100

    wibdow size correction, background with gradient

--- CONFIGS.py
+++ CONFIGS.py
@@ -11,8 +11,8 @@ else:
 # snake speed
 TIME_STEP = 0.3
 # snake theme
-# SNAKE_THEME = 'light'
-SNAKE_THEME = 'dark'
+SNAKE_THEME = 'light'
+# SNAKE_THEME = 'dark'
 
 # y shift of score label
 if platform == 'android':
@@ -21,8 +21,8 @@ else:
     SCORE_LABEL_Y = 100
 
 # add gradient to backgraound
-# IS_BACKGROUND_GRADIENT = False
-IS_BACKGROUND_GRADIENT = True
+IS_BACKGROUND_GRADIENT = False
+# IS_BACKGROUND_GRADIENT = True
 alpha_channel_rate_min = 0.7
 alpha_channel_rate_max = 1
 ALPHA_CHANNEL_RATE_FUNC = lambda x: alpha_channel_rate_min + (alpha_channel_rate_max - alpha_channel_rate_min) * (
